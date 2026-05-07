81b543c95acfde644f0fe6a45c56a030de46ade5
Thiraviyam <alaguthiraviyamofficial@gmail.com>
2026-05-07 11:09:23 +0530

    path issue fixed

--- product-frontend/src/App.js
+++ product-frontend/src/App.js
@@ -1,6 +1,6 @@
  import { BrowserRouter, Routes, Route } from 'react-router-dom';
  import SearchPage from './pages/SearchPage';
- import ProductDetailPage from './pages/ProductDetailPage';
+ import ProductDetailPage from './pages/ProductDetail';
 
  function App() {
      return ( <

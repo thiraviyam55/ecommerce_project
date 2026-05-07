9cd83a6bdfd322c37a8d54e4e74e6beb6292a10c
Thiraviyam <alaguthiraviyamofficial@gmail.com>
2026-05-07 09:43:04 +0530

    deployment setup

--- product-data/src/config/config.js
+++ product-data/src/config/config.js
@@ -2,10 +2,13 @@ require('dotenv').config();
 
 module.exports = {
     development: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        host: '127.0.0.1',
-        dialect: 'postgres' // ✅ FIX THIS
+        use_env_variable: 'DATABASE_URL',
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
     }
 };
